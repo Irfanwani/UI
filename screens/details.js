e4321d797820e347813e5339a84f218ce1848cf5
Irfan wani <irfanwani347@gmail.com>
2021-05-08 15:06:21 +0530

    editing...

--- screens/details.js
+++ screens/details.js
@@ -2,3 +2,11 @@ import React from 'react'
 import {View, Text} from 'react-native'
 
 // Details of the title here.
+
+export default function Details() {
+    return (
+        <View>
+            <Text>This is details screen</Text>
+        </View>
+    )
+}
