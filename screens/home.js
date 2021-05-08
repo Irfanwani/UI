7c6f0172f12f1cc189468f539c7afaf8ce5ad622
Irfan wani <irfanwani347@gmail.com>
2021-05-08 12:49:46 +0530

    check

--- screens/home.js
+++ screens/home.js
@@ -6,7 +6,7 @@ import {Card, FAB} from 'react-native-paper'
 
 const renderItem = ({item}) => (
     <Card style={styles.card}>
-        <Text style={{fontSize: 25}}>{item.title} dsjfgbsdjf</Text>
+        <Text style={{fontSize: 25}}>{item.title}</Text>
     </Card>
 )
 
